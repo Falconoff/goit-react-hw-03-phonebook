e8df6a2f33e20a91f3c571851761c14ac606acda
Serhii Sokolov <sokolovserega1982@gmail.com>
2022-01-19 18:26:08 +0200

    add componentDidMount and componentDidUpdate

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,13 +7,15 @@ import Filter from '../Filter';
 
 import { Container, TitleMain, TitleSecond } from './App.styled';
 
+const LS_KEY = 'contacts';
+
 class App extends Component {
   state = {
     contacts: [
       { id: '111', name: 'Michael Jackson', number: '111-11-11' },
-      { id: '222', name: 'Bob Marley', number: '222-22-22' },
-      { id: '333', name: 'Tina Turner', number: '333-33-33' },
-      { id: '444', name: 'ssv', number: '444-33-33' },
+      // { id: '222', name: 'Bob Marley', number: '222-22-22' },
+      // { id: '333', name: 'Tina Turner', number: '333-33-33' },
+      // { id: '444', name: 'ssv', number: '444-33-33' },
     ],
     filter: '',
   };
@@ -49,6 +51,41 @@ class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
   };
+
+  // Кол-во записей в Телефонной книге изменилось!!!
+  componentDidUpdate(prevProps, prevState) {
+    console.log('prevState:', prevState);
+    console.log('this.state', this.state);
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      console.log(
+        `Кол-во записей в state Телефонной книги изменилось!!! Было ${prevState.contacts.length}, стало ${this.state.contacts.length}`,
+      );
+      localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
+    }
+  }
+
+  // Первый рендер Phonebook
+  componentDidMount() {
+    console.log('componentDidMount!!!');
+    const contacts = localStorage.getItem(LS_KEY);
+    const parsedContacts = JSON.parse(contacts);
+    // console.log('contacts from localStorage:', contacts);
+    console.log('parsed contacts from localStorage:', parsedContacts);
+
+    if (parsedContacts !== null) {
+      console.log(
+        'number of parsed contacts from localStorage:',
+        parsedContacts.length,
+      );
+    } else {
+      console.log('Записей в localStorage нет!!!');
+    }
+
+    // если есть в локальном хранилище записи, записываем их в стейт
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
 
   render() {
     // for filter
